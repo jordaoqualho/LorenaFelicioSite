dc7f14483467ab3f779eef2fab41a50b324661eb
jordaoqualho <jordaoqualho@gmail.com>
2020-12-03 17:50:00 -0300

    option section

--- js/main.js
+++ js/main.js
@@ -3,3 +3,19 @@ $(window).on("load", function () {
   $("#preloader").delay(800).fadeOut("slow");
   $("body").delay(500).css({ overflow: "visible" });
 });
+
+//função que ajusta o escroll da tag "a"
+let anchorlinks = document.querySelectorAll('a[href^="#"]')
+ 
+for (let item of anchorlinks) { // relitere 
+    item.addEventListener('click', (e)=> {
+        let hashval = item.getAttribute('href')
+        let target = document.querySelector(hashval)
+        target.scrollIntoView({
+            behavior: 'smooth',
+            block: 'start'
+        })
+        history.pushState(null, null, hashval)
+        e.preventDefault()
+    })
+}
